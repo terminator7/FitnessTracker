9d68adf309608d358a40e7c5e05d3c231c170ee8
Nicholas <nalrnw@umsystem.edu>
2022-10-11 19:23:05 -0500

    Basic Button layout done for showcase in case of tomorrow '

--- pages/WeightTrackerPage.js
+++ pages/WeightTrackerPage.js
@@ -1,14 +1,73 @@
 //General imports
 import React from "react";
-import { Text, View } from 'react-native';
+import { Text, View, Button, StyleSheet, SafeAreaView, TextInput } from 'react-native';
 
 //Component Imports
 // *Insert Page imports here*
-import Button from '../components/Button';
+import Buttons from '../components/Buttons';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginHorizontal: 16,
+    },
+    fittotext: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    appButtonContainer: {
+        elevation: 8,
+        backgroundColor: "#009688",
+        borderRadius: 10,
+        paddingVertical: 10,
+        paddingHorizontal: 12,
+        margin: 7,
+      },
+      input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+      },
+});
 
 const WeightTrackerScreen = (props) => {
+    const [text, onChangeText] = React.useState("Add Here");
+    const [number, onChangeNumber] = React.useState(null);
     return (
-        <Button></Button>
+        <SafeAreaView style={styles.container}>
+
+            <View style={styles.appButtonContainer}>
+                <Button
+                    style={{fontSize: 20, color: 'green'}}
+                    color="white"
+                    onPress={() => this._handlePress()}
+                    title="Enter Weight"
+                >
+                </Button>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={onChangeText}
+                    value={text}
+                />
+            </View>
+            <View style={styles.appButtonContainer}>
+                <Button
+                    style={{fontSize: 20, color: 'green'}}
+                    color="white"
+                    onPress={() => this._handlePress()}
+                    title="Enter Units"
+                >
+
+                </Button>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={onChangeText}
+                    value={text}
+                />
+            </View>
+        </SafeAreaView>
     );
 }
 
